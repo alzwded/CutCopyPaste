74a9d09e394bd374de36b734dd204716d646416f
Vlad <alzwded@gmail.com>
2014-10-23 09:14:21 +0300

    add note

--- deploy.bat
+++ deploy.bat
@@ -6,6 +6,8 @@ IF #%PERLSITEDIR%# == ## GOTO :notDefined
 
 robocopy . "%CCPTARGET%" delete.pl edit.pl index.pl save.pl search.pl view.pl 
 
+@REM yeah, couldn't figure out what the cwd of a script actually is
+@REM so just dump that in <perl>/site/lib
 robocopy . "%PERLSITEDIR%" db.pm
 
 goto :EOF
